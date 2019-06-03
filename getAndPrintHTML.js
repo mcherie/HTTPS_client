56146ebf7550b6a72492edc62b83bdab3671aa28
Cher <red_phoenix417@yahoo.com>
2019-06-03 17:35:10 +0000

    printing with buffered strings

---
+++ getAndPrintHTML.js
@@ -0,0 +1,33 @@
+function getAndPrintHTML () {
+
+    var https = require('https'); 
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
+
+
+    https.get(requestOptions, function (response) {
+        response.setEncoding('utf8');
+        let string = ""
+        response.on('data', function (data) {
+            let part = data.toString();
+            // console.log(part);
+            string += part;
+            // console.log('stream data ', + part);
+        });
+
+        
+    response.on('end', function() {
+        console.log(string);
+    });
+
+
+
+    });
+    /* Add your code here */
+  
+  }
+
+  getAndPrintHTML ()
