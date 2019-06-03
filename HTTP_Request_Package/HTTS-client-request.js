204982f950d50d0939f6d384279ad0fa6c0e7782
Cher <red_phoenix417@yahoo.com>
2019-06-03 23:04:01 +0000

    writing an HTTPS client using request to download and image from the internet

---
+++ HTTP_Request_Package/HTTS-client-request.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               
+       .on('error', function (err) {                          
+         throw err; 
+       })
+       .on('response', function (response) {  
+           console.log('Downloading image...')                
+         console.log('Response Status Code: ', response.statusCode, 'Response Message: ', response.statusMessage, 'Content Type: ', response.headers['content-type']);
+       })
+       
+       .pipe(fs.createWriteStream('./future.jpg')) 
+       .on('finish', function () {
+        console.log('Download complete.')
+        });
+       
